b2c4da129a011e367b279785bd88480f8ab69c6b
Siegfried Goeschl <siegfried.goeschl@gmail.com>
2020-06-24 00:24:49 +0200

    FREEMARKER-146 Cleanly separate example templates and data from user-supplied content

--- freemarker-generator-tools/src/main/java/org/apache/freemarker/generator/tools/dataframe/converter/MapConverter.java
+++ freemarker-generator-tools/src/main/java/org/apache/freemarker/generator/tools/dataframe/converter/MapConverter.java
@@ -30,17 +30,6 @@ public class MapConverter {
      * Create a data frame from a list of maps. It is assumed
      * that the map represent tabular data.
      *
-     * @param map map to build the data frame
-     * @return <code>DataFrame</code>
-     */
-    public static DataFrame toDataFrame(Map<String, Object> map) {
-        return toDataFrame(singletonList(map));
-    }
-
-    /**
-     * Create a data frame from a list of maps. It is assumed
-     * that the map represent tabular data.
-     *
      * @param maps list of map to build the data frame
      * @return <code>DataFrame</code>
      */
