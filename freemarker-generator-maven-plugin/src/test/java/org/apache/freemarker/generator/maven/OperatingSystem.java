2b66124eaf819d0a8c6e8f0a956578c21057b16a
Siegfried Goeschl <siegfried.goeschl@gmail.com>
2020-07-07 13:56:58 +0200

    FREEMARKER-151 [freemarker-cli] Ensure that build and and examples are running on Windows

--- freemarker-generator-maven-plugin/src/test/java/org/apache/freemarker/generator/maven/OperatingSystem.java
+++ freemarker-generator-maven-plugin/src/test/java/org/apache/freemarker/generator/maven/OperatingSystem.java
@@ -2,8 +2,13 @@ package org.apache.freemarker.generator.maven;
 
 import java.util.Locale;
 
+/**
+ * Helper class to detect the operting system (mostly Windows).
+ *
+ * TODO should be moved to "freemarker-generator-base"
+ */
 public class OperatingSystem {
-    private static String OS = System.getProperty("os.name", "unknown").toLowerCase(Locale.ROOT);
+    private static final String OS = System.getProperty("os.name", "unknown").toLowerCase(Locale.ROOT);
 
     public static boolean isWindows() {
         return OS.contains("win");
