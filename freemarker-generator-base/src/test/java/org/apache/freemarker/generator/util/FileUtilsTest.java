66c2d8bf56b2e39ce0e9f4cc7a917a6c61e5457c
Siegfried Goeschl <siegfried.goeschl@gmail.com>
2021-09-08 19:00:06 +0200

    FREEMARKER-188 Fix broken FileUtilsTest

--- freemarker-generator-base/src/test/java/org/apache/freemarker/generator/util/FileUtilsTest.java
+++ freemarker-generator-base/src/test/java/org/apache/freemarker/generator/util/FileUtilsTest.java
@@ -1,47 +1,58 @@
 package org.apache.freemarker.generator.util;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.freemarker.generator.base.util.FileUtils;
+import org.apache.freemarker.generator.base.util.OperatingSystem;
 import org.junit.Test;
 
 import java.io.File;
 
+import static org.apache.freemarker.generator.base.util.FileUtils.getRelativePath;
 import static org.junit.Assert.assertEquals;
 
 public class FileUtilsTest {
 
     @Test
     public void shouldGetRelativePathForSameDirectory() {
-        assertEquals("", FileUtils.getRelativePath(new File("/"), new File("/pom.xml")));
-        assertEquals("", FileUtils.getRelativePath(new File("."), new File("pom.xml")));
-        assertEquals("", FileUtils.getRelativePath(new File("."), new File("./pom.xml")));
-        assertEquals("", FileUtils.getRelativePath(new File("."), new File("./pom.xml")));
-        assertEquals("", FileUtils.getRelativePath(new File("../freemarker-generator-base"), new File("pom.xml")));
-        assertEquals("", FileUtils.getRelativePath(new File("../freemarker-generator-base"), new File("./pom.xml")));
-        assertEquals("", FileUtils.getRelativePath(new File("../freemarker-generator-base"), new File("../freemarker-generator-base/pom.xml")));
+        assertEquals("", getRelativePath(new File("/"), new File("/pom.xml")));
+        assertEquals("", getRelativePath(new File("."), new File("pom.xml")));
+        assertEquals("", getRelativePath(new File("."), new File("./pom.xml")));
+        assertEquals("", getRelativePath(new File("."), new File("./pom.xml")));
+        assertEquals("", getRelativePath(new File("../freemarker-generator-base"), new File("pom.xml")));
+        assertEquals("", getRelativePath(new File("../freemarker-generator-base"), new File("./pom.xml")));
+        assertEquals("", getRelativePath(new File("../freemarker-generator-base"), new File("../freemarker-generator-base/pom.xml")));
     }
 
     @Test
     public void shouldGetRelativePathForNestedDirectory() {
-        assertEquals("src/test/data/env", FileUtils.getRelativePath(new File("."), new File("src/test/data/env/nginx.env")));
-        assertEquals("src/test/data/env", FileUtils.getRelativePath(new File("."), new File("./src/test/data/env/nginx.env")));
+        assertEquals(fixSeparators("src/test/data/env"), getRelativePath(new File("."), new File("src/test/data/env/nginx.env")));
+        assertEquals(fixSeparators("src/test/data/env"), getRelativePath(new File("."), new File("./src/test/data/env/nginx.env")));
     }
 
     @Test
     public void shouldGetRelativePathForDisjunctDirectories() {
-        assertEquals("../test/data/env", FileUtils.getRelativePath(new File("./src/site"), new File("src/test/data/env/nginx.env")));
+        assertEquals(fixSeparators("../test/data/env"), getRelativePath(new File("./src/site"), new File("src/test/data/env/nginx.env")));
     }
 
     @Test
     public void shouldHandleInvalidArgumentsGracefully() {
-        assertEquals("", FileUtils.getRelativePath(new File("."), new File(".")));
-        assertEquals("", FileUtils.getRelativePath(new File("pom.xml"), new File("pom.xml")));
-        assertEquals("", FileUtils.getRelativePath(new File("."), new File("does-not-exist.xml")));
-        assertEquals("foo", FileUtils.getRelativePath(new File("."), new File("foo/does-not-exist.xml")));
-        assertEquals("foo", FileUtils.getRelativePath(new File("."), new File("foo/./does-not-exist.xml")));
+        assertEquals("", getRelativePath(new File("."), new File(".")));
+        assertEquals("", getRelativePath(new File("pom.xml"), new File("pom.xml")));
+        assertEquals("", getRelativePath(new File("."), new File("does-not-exist.xml")));
+        assertEquals("foo", getRelativePath(new File("."), new File("foo/does-not-exist.xml")));
+        assertEquals("foo", getRelativePath(new File("."), new File("foo/./does-not-exist.xml")));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowIllegalArgumentExceptionForNonExistingDirectoy() {
-        FileUtils.getRelativePath(new File("does-not-exit"), new File("pom.xml"));
+        getRelativePath(new File("does-not-exit"), new File("pom.xml"));
+    }
+
+    private static String fixSeparators(String str) {
+        if (OperatingSystem.isWindows()) {
+            return FilenameUtils.separatorsToWindows(str);
+        } else {
+            return str;
+        }
     }
 }
